134e9675554ebf1a1e5b70f21b51fcd382ae2c5c
rgun <r.guenther.zh@gmail.com>
2021-07-17 01:17:10 +0200

    TestCaseSuite

--- subscribe_main.py
+++ subscribe_main.py
@@ -10,51 +10,10 @@ import tg_reply
 import tgdata_main
 import utilities
 from log.g3b1_log import cfg_logger
-from subscribe_token import BOT_TOKEN_SUBSCRIBE
-from utilities import TgCommand
 
 logger = cfg_logger(logging.getLogger(__name__), logging.DEBUG)
-
-
-def commands() -> dict:
-    return dict(edit=
-                TgCommand("edit", "subscribe_edit", hdl_cmd_subscribe_edit, hdl_cmd_subscribe_edit.__doc__, ['bkey']),
-                default=
-                TgCommand("default", "subscribe_default", hdl_cmd_subscribe_default, hdl_cmd_subscribe_default.__doc__,
-                          ['bkey']),
-                uname=
-                TgCommand("uname", "subscribe_uname", hdl_cmd_subscribe_uname, hdl_cmd_subscribe_uname.__doc__,
-                          ['bkey']),
-                subscribe=
-                TgCommand("subscribe", "subscribe_subscribe", hdl_cmd_subscribe_subscribe,
-                          hdl_cmd_subscribe_subscribe.__doc__, ['bkey']))
-
-
-def start(update: Update, context: CallbackContext) -> None:
-    """Displays info on how to trigger an error."""
-    tg_db.synchronize_from_message(update.message)
-
-    commands_str = utilities.build_commands_str(commands())
-    update.effective_message.reply_html(
-        commands_str +
-        utilities.build_debug_str(update)
-    )
-
-
-def hdl_message(update: Update, context: CallbackContext) -> None:
-    """store message to DB"""
-    message = update.message
-    logger.debug(f"Handle message {message}")
-    tg_db.synchronize_from_message(message)
-
-
-def lookup_lod(lod, **kw):
-    for row in lod:
-        for k, v in kw.items():
-            if row[k] != str(v): break
-        else:
-            return row
-    return None
+g3_m_str_subscribe: str = utilities.module_by_file_str(__file__)
+COLUMNS_SUBSCRIBE = []
 
 
 def map_id_uname(data_dict: dict, field_mapping: dict) -> dict:
@@ -84,12 +43,18 @@ def extract_bkey_arg(update: Update, context: CallbackContext, cmd: str) -> str:
     )
 
 
-def hdl_cmd_subscribe_uname(update: Update, context: CallbackContext) -> None:
-    """Set uname for user if key given. """
-    logger.debug(f"Insert (/edit) bot")
-    if context.args is not None and len(context.args) > 0:
-        bkey_ = extract_bkey_arg(update, context, "uname")
-        subscribe_db.set_uname(update.effective_user.id, bkey_)
+@tgdata_main.handler()
+def hdl_cmd_uname(update: Update, ctx: CallbackContext, uname: str = None) -> None:
+    """Set uname for user if key given.
+    :type ctx: object
+    :type uname: str
+    :param update:
+    :param ctx: !!!args: %uname%!!!
+    """
+    if uname:
+        logger.debug(f"Setting uname {uname}")
+        subscribe_db.set_uname(update.effective_user.id, uname)
+    logger.debug(f"Reading uname")
     uname = subscribe_db.read_uname(update.effective_user.id)
 
     update.effective_message.reply_html(
@@ -97,25 +62,35 @@ def hdl_cmd_subscribe_uname(update: Update, context: CallbackContext) -> None:
     )
 
 
-def hdl_cmd_subscribe_edit(update: Update, context: CallbackContext) -> None:
-    """Insert or save a bot."""
+@tgdata_main.handler()
+def hdl_cmd_edit(update: Update, ctx: CallbackContext, bkey: str = None) -> None:
+    """Insert or save a bot.
+    :param bkey: key of the bot = g3_m_str
+    :param ctx:
+    :param update:
+    :param ctx: !!!args: %bkey%!!!
+    """
     logger.debug(f"Insert (/edit) bot")
-    bkey_ = extract_bkey_arg(update, context, "edit")
-    if bkey_:
-        subscribe_db.bot_save(bkey_)
+    if bkey:
+        subscribe_db.bot_save(bkey)
         tg_reply.command_successful(update)
 
 
-def hdl_cmd_subscribe_default(update: Update, context: CallbackContext) -> None:
-    """Set the default bot for the user."""
-    logger.debug(f"Set default bot")
-    bkey_ = extract_bkey_arg(update, context, "default")
-    if bkey_:
-        subscribe_db.bot_default(update.effective_chat.id, update.effective_user.id, bkey_)
+@tgdata_main.handler()
+def hdl_cmd_default(update: Update, ctx: CallbackContext, bkey: str = None) -> None:
+    """Set the default bot for the user.
+    :param bkey: key of the bot / g3_m_str
+    :param update:
+    :param ctx: !!!args: %bkey%!!!
+    """
+    logger.debug(f"Set default bot to {bkey}")
+    if bkey:
+        subscribe_db.bot_default(update.effective_chat.id, update.effective_user.id, bkey)
         tg_reply.command_successful(update)
 
 
-def hdl_cmd_subscribe_subscribe(update: Update, context: CallbackContext) -> None:
+@tgdata_main.handler()
+def hdl_cmd_subscribe(update: Update, ctx: CallbackContext, bkey: str = None) -> None:
     """Subscribe to a bot %bkey% with current user and chat."""
     logger.debug(f"Subscribe to a bot")
 
@@ -124,17 +99,15 @@ def hdl_cmd_subscribe_subscribe(update: Update, context: CallbackContext) -> Non
     user_id = update.effective_user.id
     subscribe_db.for_user(user_id)
 
-    bkey_ = extract_bkey_arg(update, context, "subscribe")
-    if bkey_:
-        subscribe_db.bot_activate(user_id, chat_id, bkey_)
+    if bkey:
+        subscribe_db.bot_activate(user_id, chat_id, bkey)
         tg_reply.command_successful(update)
 
 
 def main() -> None:
     """Run the bot."""
     # str(bot_key): dict(db_row)
-
-    tgdata_main.start_bot(__file__, commands(), start, hdl_message)
+    tgdata_main.start_bot(__file_)
 
 
 if __name__ == '__main__':
--- subscribe_test.py
+++ subscribe_test.py
@@ -14,23 +14,9 @@ import subscribe_token
 import test_utils
 import utilities
 from log.g3b1_log import cfg_logger
-from utilities import print_header_line, get_module_name
+from utilities import print_header_line, module_by_file_str
 
 logger = cfg_logger(logging.getLogger(__name__), logging.DEBUG)
-
-
-class MyMessage(Message):
-
-    def reply_html(self, text: str, disable_web_page_preview: ODVInput[bool] = DEFAULT_NONE,
-                   disable_notification: DVInput[bool] = DEFAULT_NONE, reply_to_message_id: int = None,
-                   reply_markup: ReplyMarkup = None, timeout: ODVInput[float] = DEFAULT_NONE,
-                   api_kwargs: JSONDict = None, allow_sending_without_reply: ODVInput[bool] = DEFAULT_NONE,
-                   entities: Union[List['MessageEntity'], Tuple['MessageEntity', ...]] = None,
-                   quote: bool = None) -> 'Message':
-        print(text)
-        return self
-        # return super().reply_html(text, disable_web_page_preview, disable_notification, reply_to_message_id,
-        #                          reply_markup, timeout, api_kwargs, allow_sending_without_reply, entities, quote)
 
 
 def test_subscribe(update: Update, ctx: CallbackContext):
@@ -39,31 +25,20 @@ def test_subscribe(update: Update, ctx: CallbackContext):
     print(bot_all)
     print(f'todo bot: {bot_all["todo"]}')
 
-    ctx.args = ['todo']
-    print_header_line("cmd subsribe %todo%")
-    subscribe_main.hdl_cmd_subscribe_subscribe(update, ctx)
+    print_header_line("cmd subscribe %todo%")
+    subscribe_main.hdl_cmd_subscribe(update, ctx, bkey='todo')
 
     print_header_line("cmd uname %user_1%")
-    ctx.args = ['uname_1']
-    subscribe_main.hdl_cmd_subscribe_uname(update, ctx)
+    subscribe_main.hdl_cmd_uname(update, ctx, uname='uname_1')
 
     print_header_line("cmd uname %user_2%")
-    update, ctx = test_utils.setup(utilities.get_module(), 2, 1, 2, 'user-2')
-    ctx.args = ['uname_2']
-    subscribe_main.hdl_cmd_subscribe_uname(update, ctx)
+    update, ctx = test_utils.setup(subscribe_main.__file__, 2, 1, 2, 'user-2')
+    subscribe_main.hdl_cmd_uname(update, ctx, uname='uname_2')
 
 
 def main():
-    module = utilities.get_module()
-
-    message = MyMessage(message_id=1, date=datetime.now(),
-                        chat=Chat(id=1, type=constants.CHAT_GROUP),
-                        from_user=User(id=1, first_name='GUNNAR', is_bot=False)
-                        )
-    update = Update(1, message)
-    dispatcher = Dispatcher(Bot(subscribe_db.bot_all()[module]['token']), Queue())
-    context = CallbackContext(dispatcher)
-    test_subscribe(update, context)
+    update, ctx = test_utils.setup(subscribe_main.__file__)
+    test_subscribe(update, ctx)
 
 
 if __name__ == '__main__':
